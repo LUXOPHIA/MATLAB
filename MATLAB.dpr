92fda53ba6f7b4bf6f4ec0343fd7ae49966f56c0
luxidea <contact@luxidea.net>
2018-05-12 03:35:04 +0900

    no message

--- MATLAB.dpr
+++ MATLAB.dpr
@@ -3,7 +3,10 @@
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Main in 'Main.pas' {Form1};
+  Main in 'Main.pas' {Form1},
+  MATLAB.engine in '__LIBRARY\LUXOPHIA\LUX.MATLAB\MATLAB.engine.pas',
+  LUX.Code.C in '..\..\_LIBRARY\・LUX.Code\LUX.Code.C.pas',
+  MATLAB.matrix in '__LIBRARY\LUXOPHIA\LUX.MATLAB\MATLAB.matrix.pas';
 
 {$R *.res}
 
